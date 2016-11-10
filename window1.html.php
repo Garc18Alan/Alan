b27f77a997800274757a7559c2d5fa7114803cb9
Garc18Alan <a.gaarcia@hotmail.com>
2016-11-10 12:02:17 -0200

    Create window1.html.php

---
+++ window1.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Página Window1</title>
+</head>
+<body>
+
+	<h1>Bom dia, <?php echo $nome ?>!</h1>
+	<h1>Senha: <?php echo $senha ?></h1>
+
+</body>
+</html>
